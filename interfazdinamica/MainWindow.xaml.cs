b5d198ea9312402c7bc938bcd4ac053520aaafba
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-09-30 12:53:15 -0700

    Agregar archivos de proyecto.

---
+++ interfazdinamica/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace interfazdinamica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            BtnAceptar.Visibility = Visibility.Hidden;
+            BtnCancelar.Visibility = Visibility.Hidden;
+        }
+
+        private void CbPersona_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grdParametrosPersona.Children.Clear();
+            
+
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (CbPersona.SelectedIndex)
+            {
+                case 0:  //Alumno
+                    grdParametrosPersona.Children.Add(new Alumno());
+                    BtnAceptar.Visibility = Visibility.Visible;
+                    BtnCancelar.Visibility = Visibility.Visible;
+                    break;
+
+                case 1:  //Maestro
+                    grdParametrosPersona.Children.Add(new Maestro());
+                    BtnAceptar.Visibility = Visibility.Visible;
+                    BtnCancelar.Visibility = Visibility.Visible;
+                    break;
+            }
+
+
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            grdParametrosPersona.Children.Clear();
+            BtnAceptar.Visibility = Visibility.Hidden;
+            BtnCancelar.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+            grdParametrosPersona.Children.Clear();
+            BtnAceptar.Visibility = Visibility.Hidden;
+            BtnCancelar.Visibility = Visibility.Hidden;
+        }
+    }
+}
